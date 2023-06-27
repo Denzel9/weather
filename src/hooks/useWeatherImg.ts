31e86bc0239ce6c7e14682bf0456dc51f0ad571a
Denzel9 <denisnikitin9797@gmail.com>
2023-06-27 22:56:46 +0300

    fix

--- src/hooks/useWeatherImg.ts
+++ src/hooks/useWeatherImg.ts
@@ -5,13 +5,18 @@ export const useWeatherImg = (data: string) => {
   const [img, setImg] = useState('')
   const searchImg = () => {
     if (data === 'Ясно' || data === 'Чисто' || data === 'Солнечно') {
-      return currentHour > '22' || currentHour < '07' ? setImg('bg-Night') : setImg('bg-Sunny')
+      return currentHour >= '22' || currentHour <= '07' ? setImg('bg-Night') : setImg('bg-Sunny')
     }
-    if (data === 'Дождь' || data === 'Местами дождь' || data === 'Пасмурно') {
-      return currentHour > '22' || currentHour < '07' ? setImg('bg-Night') : setImg('bg-Razor')
+    if (
+      data === 'Дождь' ||
+      data === 'Местами дождь' ||
+      data === 'Пасмурно' ||
+      data === 'В отдельных районах умеренный или сильный дождь с грозой'
+    ) {
+      return currentHour >= '22' || currentHour <= '07' ? setImg('bg-Night') : setImg('bg-Razor')
     }
     if (data === 'Облачно' || data === 'Переменная облачность') {
-      return currentHour > '22' || currentHour < '07' ? setImg('bg-Night') : setImg('bg-Cloudly')
+      return currentHour >= '22' || currentHour <= '07' ? setImg('bg-Night') : setImg('bg-Cloudly')
     }
   }
 
