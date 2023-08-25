edf01b5bf3db60e830f42e4265af90978ead1790
Denzel9 <denisnikitin9797@gmail.com>
2023-08-25 22:43:17 +0300

    done size /

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,16 +6,18 @@ import { useWeatherImg } from '../../hooks/useWeatherImg'
 import { DataContext } from '../../context/DataContextProvider'
 import MobileNav from './navigation/MobileNav'
 import Navigation from './navigation/Navigation'
+import { useLocation } from 'react-router-dom'
 
 const Layout: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
   const { data, isLoading } = useContext(DataContext)
   const { img } = useWeatherImg(data)
-
+  const { pathname } = useLocation()
   return (
     <div
       className={classNames(
         img,
-        'text-white h-screen bg-cover bg-center max-sm:h-full max-sm:pb-10 '
+        pathname === '/' && 'max-sm:h-full',
+        'text-white h-screen bg-cover bg-center max-sm:pb-10 '
       )}
     >
       <CurrentInfo data={data?.weather!} isLoading={isLoading} />
