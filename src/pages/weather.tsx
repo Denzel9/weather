bf24e51788a39a40557618323d82ed2072f9189e
Denzel9 <denisnikitin9797@gmail.com>
2024-05-24 14:59:28 +0300

    1

--- src/pages/weather.tsx
+++ src/pages/weather.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react'
 import HomePage from '../components/screens/home/HomePage'
 
 const Weather: FunctionComponent = () => {
+  const a = 2
   return <HomePage />
 }
 
